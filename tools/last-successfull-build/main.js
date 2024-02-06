50b1eb6356492b8503cb61424bbd036638b1d4a0
Saif Jerbi <saifjerbi@MacBook-Air.local>
2024-02-06 15:19:00 +0100

    update front

---
+++ tools/last-successfull-build/main.js
@@ -0,0 +1,121 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require('child_process');
+const { promisify } = require('util');
+
+const execAsync = promisify(exec);
+
+let repoShas;
+const verifyCommit = async (sha) => {
+  if (!repoShas) {
+    try {
+      const cmd = `git log --format=format:%H`;
+      core.info(`Getting list of SHAs in repo via command "${cmd}"`);
+
+      const { stdout } = await execAsync(cmd);
+
+      repoShas = stdout.trim().split('\n');
+    } catch (e) {
+      repoShas = [];
+      core.warning(`Error while attempting to get list of SHAs: ${e?.message}`);
+
+      return false;
+    }
+  }
+
+  core.info(`Looking for SHA ${sha} in repo SHAs`);
+
+  return repoShas.includes(sha);
+};
+
+async function run() {
+  try {
+    const inputs = {
+      token: core.getInput('token'),
+      branch: core.getInput('branch'),
+      workflow: core.getInput('workflow'),
+      verify: core.getInput('verify') === 'true' ? true : false,
+    };
+
+    const octokit = github.getOctokit(inputs.token);
+    const repository = process.env.GITHUB_REPOSITORY;
+    const [owner, repo] = repository.split('/');
+
+    const workflows = await octokit.rest.actions.listRepoWorkflows({
+      owner,
+      repo,
+    });
+    const workflowId = workflows.data.workflows.find(
+      (w) => w.name.toLowerCase() === inputs.workflow.toLowerCase()
+    )?.id;
+
+    if (!workflowId) {
+      core.setFailed(`No workflow exists with the name "${inputs.workflow}"`);
+      return;
+    } else {
+      core.info(`Discovered workflowId for search: ${workflowId}`);
+    }
+
+    const response = await octokit.rest.actions.listWorkflowRuns({
+      owner,
+      repo,
+      workflow_id: workflowId,
+      per_page: 100,
+    });
+    const runs = response.data.workflow_runs
+      .filter(
+        (x) =>
+          (!inputs.branch || x.head_branch === inputs.branch) &&
+          x.conclusion === 'success'
+      )
+      .sort(
+        (r1, r2) =>
+          new Date(r2.created_at).getTime() - new Date(r1.created_at).getTime()
+      );
+
+    let triggeringSha = process.env.GITHUB_SHA;
+    let sha = undefined;
+
+    if (runs.length > 0) {
+      for (const run of runs) {
+        core.debug(`This SHA: ${triggeringSha}`);
+        core.debug(`Run SHA: ${run.head_sha}`);
+        core.debug(`Run Branch: ${run.head_branch}`);
+        core.debug(`Wanted branch: ${inputs.branch}`);
+
+        if (inputs.branch && run.head_branch !== inputs.branch) {
+          continue;
+        }
+
+        if (inputs.verify && !(await verifyCommit(run.head_sha))) {
+          core.warning(`Failed to verify commit ${run.head_sha}. Skipping.`);
+          continue;
+        }
+
+        core.info(
+          inputs.verify
+            ? `Commit ${run.head_sha} from run ${run.html_url} verified as last successful CI run.`
+            : `Using ${run.head_sha} from run ${run.html_url} as last successful CI run.`
+        );
+        sha = run.head_sha;
+
+        break;
+      }
+    } else {
+      core.info(`No previous runs found for branch ${inputs.branch}.`);
+    }
+
+    if (!sha) {
+      core.warning(
+        'Unable to determine SHA of last successful commit. Using SHA for current commit.'
+      );
+      sha = triggeringSha;
+    }
+
+    core.setOutput('sha', sha);
+  } catch (error) {
+    core.setFailed(error?.message);
+  }
+}
+
+run();
